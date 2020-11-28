454eee206dd6c53a4e65e9cb644d4fcf5fc72a22
grenook-prog <artem.frizen02@gmail.com>
2020-11-28 13:09:46 +0600

    Updates

---
+++ precache-manifest.5d3590b786da97086d9151dd4ea5eb71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb9c452dbabacff6427b3933953e215",
+    "url": "/First-react-app/index.html"
+  },
+  {
+    "revision": "a18326fa0dcf7883a7c9",
+    "url": "/First-react-app/static/css/main.7550e85e.chunk.css"
+  },
+  {
+    "revision": "1468426418fec02fc0dc",
+    "url": "/First-react-app/static/js/2.fcabcc02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/First-react-app/static/js/2.fcabcc02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a18326fa0dcf7883a7c9",
+    "url": "/First-react-app/static/js/main.db5d6692.chunk.js"
+  },
+  {
+    "revision": "5b271ee517c5788f613f",
+    "url": "/First-react-app/static/js/runtime-main.1b9fe7a1.js"
+  }
+]);
